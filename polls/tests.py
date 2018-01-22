6749589cb420bec436aa94df03ab9d8fcf5e00bb
jkingz <jfkrule@gmail.com>
2018-01-22 14:11:57 +0800

    Add polls test sample

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
-# Create your tests here.
+"""sample test"""
+import unittest
+
+from polls import hello
+
+
+class TestHello(unittest.TestCase):
+    """sample test"""
+
+    def test_world(self):
+        """sample test"""
+        self.assertEqual(hello('world'), 'hello world')
+
+    def test_world_unicode(self):
+        """sample test with unicode"""
+        self.assertEqual(hello(u'world'), u'hello world')
